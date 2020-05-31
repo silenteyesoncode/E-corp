a8421d95bef9cfce22c5bc5a259efa050533d862
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-05-31 23:41:02 +0530

    router and switch
    hatsPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    HATS PAGE
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
