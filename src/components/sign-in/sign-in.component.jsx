feba934b7e1c1124ae9b1c3e6ffbe8422ad4fcd5
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-06-03 12:19:41 +0530

    custom_button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 
@@ -47,7 +48,7 @@ class SignIn extends React.Component{
             label='password'
             required
           />
-          {/* <CustomButton type='submit'> Sign in </CustomButton> */}
+          <CustomButton type='submit'> Sign in </CustomButton>
         </form>
       </div>
     );
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -4,7 +4,6 @@ import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
-    signIn
     <SignIn />
   </div>
 );
