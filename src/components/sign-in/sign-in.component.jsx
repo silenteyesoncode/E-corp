e1c3cc4bd293e097ee992afdd9302c08266f2c48
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-06-04 16:17:23 +0530

    sign-on(google)

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,10 +49,12 @@ class SignIn extends React.Component{
             label='password'
             required
           />
-          <CustomButton type='submit'> Sign in </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-           {' '} Sign in with google {' '}
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn >
+            {' '} Sign in with google {' '}
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
