fbdde7fa457649ca32c0883a0b6a051c07d1a36f
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-06-12 20:04:57 +0530

    directory_redux
    bring directory state into redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,60 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.style.scss';
 
-class Directory extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: ''
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: ''
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/PZCPMzM/photo-storyb853a2cef6ef951724f8f3960fba9496-0.jpg',
-          size: 'large',
-          id: 4,
-          linkUrl: ''
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/0CJW10s/35303815-1139510179523834-1338673162212081664-o.jpg',
-          size: 'large',
-          id: 5,
-          linkUrl: ''
-        }
-      ]
-    };
-  }
+const Directory = ({ sections }) =>(
+  <div className='directory-menu'>
+    {sections.map(({ id,  ...otherSection}) => (
+      <MenuItem key={id} {...otherSection} />
+    ))}
+  </div>
+);
+   
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id,  ...otherSection}) => (
-          <MenuItem key={id} {...otherSection} />
-        ))}
-      </div>
-    );
-  }
-}
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -33,4 +33,14 @@ const INITIAL_STATE = {
       linkUrl: ''
     }
   ]
+};
+
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
 }
+
+export default directoryReducer;
