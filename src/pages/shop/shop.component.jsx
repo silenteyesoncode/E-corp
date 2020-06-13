5682b3f394ec8d4c6070e481c391163923a1c2e2
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2020-06-14 03:42:57 +0530

    data_And_selectors-modification

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-// import CollectionPage from '../collection/collection.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionsOverview/>
-    {/* <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} /> */}
+    {/* {match-path} :- this make it more flexible if we wanted to reuse it in another place! */}
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,4 +8,37 @@ export const selectCollections = createSelector(
     shop => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+// Data Normalisation
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+);
+
+
+// when our data is stored in array
+// also this used with find()
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5}
+
+// export const selectCollection = collectionUrlParam =>
+// createSelector(
+//  [selectCollections],
+//    collections => collections.find(
+//     collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]));
+// 
+// find() returns our collectionId although its compramise with time-complexity
+
+
+
+
 
